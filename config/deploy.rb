3a527fedafa18082ba33d03ca4c93e050081789c
Aaron Quint <aaron@quirkey.com>
2011-09-27 12:29:59 +0100

    Fixing deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ default_run_options[:pty] = true
 
 set :application, "statsd"
 set :deploy_to, '/var/daemons/statsd'
+set :deploy_via, :remote_cache
 set :scm, :git
 set :repository,  "git://github.com/paperlesspost/statsd.git"
 set :user, "paperless"
@@ -12,12 +13,24 @@ set :normalize_asset_timestamps, false
 
 namespace :statsd do
   task :install_modules do
-    run 'sudo npm install service'
+    run "cd #{shared_path} && npm install service"
+    run "ln -nfs #{shared_path}/node_modules #{current_path}/node_modules"
   end
 
   task :write_config do
     put ERB.new(File.read("config.js.erb")).result(binding), "#{release_path}/config.js", :via => :scp
   end
+end
+
+task :production do
+  role :app, 'statsd01.pp.local', :primary => true
+
+  set :graphite_port, 2003
+  set :graphite_host, '10.0.0.242'
+  set :statsd_port, 8125
+  set :lock_file, lambda { "#{shared_path}/pids/statsd.pid" }
+  set :log_file, lambda { "#{shared_path}/log/statsd.log" }
+  set :flush_interval, 10000
 end
 
 task :staging do
@@ -32,16 +45,12 @@ task :staging do
 end
 
 namespace :deploy do
- task :start do
-   run "#{current_path}/stats.js '#{current_path}/config.js' start"
- end
- task :stop do
-   run "#{current_path}/stats.js '#{current_path}/config.js' stop"
- end
- task :restart, :roles => :app, :except => { :no_release => true } do
-   run "#{current_path}/stats.js '#{current_path}/config.js' restart"
- end
+ [:start, :stop, :restart].each do |command|
+   task command, :roles => :app, :except => { :no_release => true } do
+     run "cd #{current_path} && ./stats.js '#{current_path}/config.js' #{command}"
+   end
+  end
 end
 
 after "deploy:update_code", "statsd:write_config"
-after "deploy:update_code", "statsd:install_modules"
+after "deploy:symlink", "statsd:install_modules"
